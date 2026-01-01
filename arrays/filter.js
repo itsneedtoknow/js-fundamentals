b6d9f2436d166e4f3066b5efe1c6a1bd3005b909
itsneedtoknow <letta_in_love@mail.ru>
2026-01-01 16:06:40 +0300

    feat: add filter exercise

--- arrays/filter.js
+++ arrays/filter.js
@@ -9,11 +9,29 @@ let result = numbers
 
 //Задача:
 //получить массив имён (name) только активных пользователей.
-const users = [
+const usersEng = [
   { id: 1, name: 'Anna', active: true },
   { id: 2, name: 'Bob', active: false },
   { id: 3, name: 'Kate', active: true },
 ];
-let activeUsersNames = users
+let activeUsersNames = usersEng
 .filter(item => item.active)
 .map(item => item.name);
+
+// Задачи:
+//Получить массив имён только активных пользователей.
+//Получить список пользователей, зарегистрировавшихся после 1 октября 2025.
+//Посчитать общее количество активных пользователей.
+const users = [
+  { name: "Анна", active: true, registrationDate: "2025-10-01" },
+  { name: "Иван", active: false, registrationDate: "2025-09-28" },
+  { name: "Мария", active: true, registrationDate: "2025-10-03" }
+];
+
+let activeUsers = users.filter(item => item.active);
+let registeredUsers = users.filter(item => {
+  let registrDate= new Date(item.registrationDate);
+  let deadLineDate = new Date('2025-10-01');
+  return registrDate  > deadLineDate;
+} )
+let totalActive = activeUsers.length;
