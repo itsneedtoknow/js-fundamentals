a4188985cf5540bc50848b9f9f61db0e3424977b
itsneedtoknow <letta_in_love@mail.ru>
2026-01-08 13:24:04 +0300

    feat: add call/bind/apply exercises

--- functions/bind-call-apply.js
+++ functions/bind-call-apply.js
@@ -11,3 +11,18 @@ const personOne = {
 
 }
 console.log(logPerson.bind(personOne)())
+
+
+// вызови greet так, чтобы она вывела "Привет, Анна!" используя call
+const user = {
+  name: 'Анна',
+  age: 28
+};
+
+function greet(greeting, punctuation) {
+  console.log(`${greeting}, ${this.name}${punctuation}`);
+}
+greet.call(user, 'Привет' , "!")
+greet.apply(user, ['Добрый день', "!"])
+let sayHi = greet.bind(user, "Привет")
+sayHi(')))')
