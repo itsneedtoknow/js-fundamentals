c59b9928ed837473962b34e73224874fd09ce1b1
itsneedtoknow <letta_in_love@mail.ru>
2026-01-19 10:06:44 +0300

    feat: add closures exercise

--- functions/closures.js
+++ functions/closures.js
@@ -119,4 +119,35 @@ function sum(a){
     }
 }
 let ssum = sum(1);
-console.log(ssum(2))
+console.log(ssum(2));
+
+// У нас есть встроенный метод arr.filter(f) для массивов. 
+// Он фильтрует все элементы с помощью функции f. 
+// Если она возвращает true, то элемент добавится в возвращаемый массив.
+//Сделайте набор «готовых к употреблению» фильтров:
+//inBetween(a, b) – между a и b (включительно).
+//inArray([...]) – находится в данном массиве.
+//Они должны использоваться таким образом:
+//arr.filter(inBetween(3,6)) – выбирает только значения между 3 и 6 (включительно).
+//arr.filter(inArray([1,2,3])) – выбирает только элементы, совпадающие с одним из элементов массива
+
+function inBetween(c, d){
+
+    return function(h){
+        if(h >= c && h <= d){
+            return h
+        }
+
+    }
+
+}
+let arr = [1, 2, 3, 4, 5, 6, 7];
+console.log( arr.filter(inBetween(3, 6)) );
+
+function inArray(arrayy){
+
+    return function(h){
+       return arrayy.includes(h);
+    }
+}
+console.log( arr.filter(inArray([1, 2, 10])) )
